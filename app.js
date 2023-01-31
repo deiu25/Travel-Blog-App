f990606849059e1b32b88ee7bfed0f7a5ec67bf8
deiu25 <d3iu25@gmail.com>
2023-01-31 22:28:07 +0200

    Connecting To MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
-app.use("/", (req, res, next) => {
-  return res.send("Hi");
-});
+// middlewares
 
-app.listen(5000, () => console.log("Listening to localhost Port 5000"));
+// connections
+mongoose
+  .connect(
+    `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.vuiorgi.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() =>
+    app.listen(5000, () =>
+      console.log("Connection Succesfull Listening to localhost Port 5000")
+    )
+  )
+  .catch((err) => console.log(err));
+
+//mongoDb password
+//HxJ-PJUq8wngG.L
