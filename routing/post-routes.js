a51cf03e0720b702f37e149eeb8975bb9aa0c49a
deiu25 <d3iu25@gmail.com>
2023-02-01 22:07:19 +0200

    Moving Into Posts

---
+++ routing/post-routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { addPost, getAllPosts } from "../controllers/post-controller";
+
+const postRouter = Router();
+
+postRouter.get("/", getAllPosts);
+postRouter.post("/", addPost);
+
+export default postRouter;
