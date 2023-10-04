4462512c073fa55c28844dad5a67fc1f60f007b9
deiu25 <deiu25@gmail.com>
2023-10-04 13:50:05 +0300

    initial commit

---
+++ routing/post-routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { addPost, deletePost, getAllPosts, getPostById, updatePost } from "../controllers/post-controller";
+import { protect } from "../middleware/authMiddleware";
+
+const postRouter = Router();
+
+postRouter.get("/", getAllPosts);
+postRouter.get("/:id", getPostById);
+
+// Adaugă protect middleware înainte de controller
+postRouter.post("/", protect, addPost);
+postRouter.put("/:id", protect, updatePost);
+postRouter.delete("/:id", protect, deletePost);
+
+export default postRouter;
